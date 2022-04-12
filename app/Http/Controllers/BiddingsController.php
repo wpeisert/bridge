ac0ab9028ef3a0a7b363bc0e4a8ea866d241879b
Wojciech Peisert <wojtekp@sauron.pl>
2022-04-12 13:33:54 +0200

    fix ns

--- app/Http/Controllers/BiddingsController.php
+++ app/Http/Controllers/BiddingsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\BiddingRepositoryInterface;
+use App\Interfaces\BiddingRepositoryInterface;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
--- app/Repositories/BiddingRepository.php
+++ app/Repositories/BiddingRepository.php
@@ -2,7 +2,7 @@
 
 namespace App\Repositories;
 
-use App\Services\BiddingRepositoryInterface;
+use App\Interfaces\BiddingRepositoryInterface;
 use App\Models\User;
 
 class BiddingRepository implements BiddingRepositoryInterface

7d386ee530fbb2aa97a2f9d69a0470d092e5bda5
Wojciech Peisert <wojtekp@sauron.pl>
2022-04-15 14:53:13 +0200

    cleanup

--- app/Services/DealBuilder/DealConstraintVerifier.php
+++ app/Services/DealBuilder/DealConstraintVerifier.php
@@ -3,25 +3,17 @@
 namespace App\Services\DealBuilder;
 
 use App\BridgeCore\Tools;
-use App\Services\DealConstraint\DealConstraintDecoratorFactoryInterface;
 use App\Services\DealDecorator\DealDecoratorFactoryInterface;
-use App\Services\DealDecorator\DealDecoratorInterface;
 use App\Models\Deal;
 use App\Models\DealConstraint;
 
 class DealConstraintVerifier implements DealConstraintVerifierInterface
 {
-    private DealDecoratorInterface $deal;
-
-    public function __construct(
-        private DealDecoratorFactoryInterface $dealDecoratorFactory,
-        private DealConstraintDecoratorFactoryInterface $dealConstraintDecoratorFactory
-    ) {}
+    public function __construct(private DealDecoratorFactoryInterface $dealDecoratorFactory) {}
 
     public function verify(Deal $deal, DealConstraint $dealConstraint): bool
     {
         $dealDecorator = $this->dealDecoratorFactory->decorate($deal);
-        $dealConstraintDecorator = $this->dealConstraintDecoratorFactory->decorate($dealConstraint);
 
         foreach (array_keys(Tools::getDealConstraintFields()) as $field) {
             $split = explode('_', $field);

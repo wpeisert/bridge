64267badb7f47fc47c86c6b96b5b563215c66fc5
Wojciech Peisert <wojtekp@sauron.pl>
2022-05-09 11:47:53 +0200

    fix

--- public/index.php
+++ public/index.php
@@ -48,6 +48,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
+phpinfo(); exit;
+
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();

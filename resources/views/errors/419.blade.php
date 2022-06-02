095dc4aaaded96702f2476b3bf903202ad20fd8a
Wojciech Peisert <wojtekp@sauron.pl>
2022-06-02 15:38:22 +0200

    handle 419

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,7 @@
+<x-errors-layout>
+
+    <x-slot name="title">{{ __('Page Expired') }}</x-slot>
+    <x-slot name="code">{{ __('419') }}</x-slot>
+    <x-slot name="message">{{ __('Page Expired') }}</x-slot>
+
+</x-errors-layout>

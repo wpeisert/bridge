d022d6d732ad7f2fbc20cf77efce276d9b29be31
Wojciech Peisert <wojtekp@sauron.pl>
2023-01-13 13:55:25 +0100

    Enable emails

--- resources/views/emails/bid_expected/html.blade.php
+++ resources/views/emails/bid_expected/html.blade.php
@@ -1,8 +1,8 @@
 Hi.
 <br /><br />
 
-Bid required in your bidding:
-<a href="{{ url(route('biddings.edit',$bidding->id)) }}">here</a>
+Bid required in your bidding
+<a href="{{ url(route('mybidding',$bidding->id)) }}">here</a>
 
 <br /><br />
 Cheers!
